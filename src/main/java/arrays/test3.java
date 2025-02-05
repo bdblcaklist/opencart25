cb6868fbf69e4f1b3be808fd8d7a8407f8f38c30
bdblacklist <motiurny@gmail.com>
2025-02-04 19:38:46 -0500

    Java for Loop

---
+++ src/main/java/arrays/test3.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		
+		
+
+		for(int i = 0; i<=0; i++) {
+			
+			System.out.println("I love Automation");
+			System.out.println("Manual");
+			System.out.println("Data Base");
+			
+		
+		
+		}
+
+	}
+
+}
