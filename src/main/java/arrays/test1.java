96d85f264ec9d977448c20ad62c5b935609c95f8
bdblacklist <motiurny@gmail.com>
2025-02-04 19:02:23 -0500

    This is my first commit to Java

---
+++ src/main/java/arrays/test1.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		int a [] [] = {{100,200},
+					{300,400},
+					{500,600}
+					};
+		
+		System.out.println(a.length);
+		System.out.println(a [1].length);
+
+	}
+
+}
